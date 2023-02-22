d8b1c241a54991a098c8d835017438d7fc5ac64b
José Molina <jmolina@bolsadesantiago.com>
2023-02-22 11:50:29 -0300

    upgrade jquery

--- build.sh
+++ build.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-docker image rm registry.bcs.cl/auna/auna-docs:nginx
-docker build --no-cache -t registry.bcs.cl/auna/auna-docs:latest .
-docker push registry.bcs.cl/auna/auna-docs:latest
+repo="${BUILD_REPO}"
+tag="${BUILD_TAG}"
+
+if [ -z "${repo}" ]
+then
+    repo="registry.aunablockchain.com/auna"
+fi
+if [ -z "${tag}" ]
+then
+    tag="latest"
+fi
+
+echo "Building image"
+docker build -t $repo/auna-docs:$tag --compress .
+
+echo ""
+echo "Pushing image"
+docker push $repo/auna-docs:$tag
